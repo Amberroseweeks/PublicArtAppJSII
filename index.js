a1ace90e80907d097fe427a0d17e8923169a2429
Amberroseweeks <weeksamberrose@gmail.com>
2020-07-23 19:39:29 -0400

    Ambers Changes

--- index.js
+++ index.js
@@ -1,4 +1,37 @@
 
+let artArray = [];
+let athleticArtArray = [];
+let medicalArtArray = [];
+let warArtArray = [];
+let exhibitArray = [];
+
+//
+// AW-EMPTY ARRAY FOR THE FILTERED CATEGORY 
+
+let athleticArtArrayLocation = [];
+let medicalArtArrayLocation = [];
+let warArtArrayLocation = [];
+
+// AW-BUTTONS FOR THE CATEGORY 
+
+let athleticButton = document.getElementById("athletic")
+let medicalButton = document.getElementById("medical")
+let warButton = document.getElementById("war")
+
+athleticButton.addEventListener ("click", thisWorks)
+medicalButton.addEventListener ("click", thisWorks)
+warButton.addEventListener ("click", thisWorks)
+
+// AW-TEST
+
+function thisWorks() {
+    console.log("This works!")
+}
+
+
+window.addEventListener('load', initMap) 
+
+
     
         function viewCompleteArtistData () {
 //            made a for loop for all the listed artists within the database, i used the artits's id numbers
@@ -80,18 +113,146 @@
             }
         }
 
+
+function getartData (){
+    $.ajax({
+        url: "https://raw.githubusercontent.com/Amberroseweeks/PublicArtAppJSII/emily/phlPublicArt.json",
+        success: function(artDataUnparsed) {
+           let artData = JSON.parse(artDataUnparsed)
+           console.log(artData)
+        
+             for(let i = 0; i < artData.length; i++) {
+                 
+                artArray.push(artData[i]) 
+    // AW-THIS VERY COMPLICATED LOOP FILTERS THROUGH THE ELEMENTS THAT DON'T HAVE DATA AND MAKES SURE THEY ARE WAR, ATHLETIC, OR //MEDICAL             
+                 
+                if (artData[i].exhibits === undefined || artData[i].exhibits == null || artData[i].exhibits <= 0 || artData[i].artists === undefined || artData[i].artists == null || artData[i].artists <= 0 || artData[i].tours === undefined || artData[i].tours == null || artData[i].tours <= 0 || artData[i].years === undefined || artData[i].years == null || artData[i].years <= 0) {
+                console.log("n/a");
+                } else if (artData[i].exhibits[0].name === "Athletic"){
+                        console.log(artData[i].exhibits[0].name + " " + artData[i].artists[0].name + " " + artData[i].pictures[0].large.url + " " + artData[i].title.display + " " + artData[i].location.latitude + " " + artData[i].location.longitude + " " + artData[i].tours[0].name + " " + artData[i].years[0].year);
+                        
+                    exhibitArray.push(artData[i].exhibits[0].name + " " + artData[i].artists[0].name + " " + artData[i].pictures[0].large.url + " " + artData[i].title.display + " " + artData[i].location.latitude + " " + artData[i].location.longitude + " " + artData[i].tours[0].name + " " + artData[i].years[0].year);
+                    
+                    } 
+                 else if (artData[i].exhibits[0].name === "Medical"){
+                        console.log(artData[i].exhibits[0].name + " " + artData[i].artists[0].name + " " + artData[i].pictures[0].large.url + " " + artData[i].title.display + " " + artData[i].location.latitude + " " + artData[i].location.longitude + " " + artData[i].tours[0].name + " " + artData[i].years[0].year);
+                     
+                        medicalExhibit = artData[i].exhibits[0].name
+                        medicalArtistName = artData[i].artists[0].name
+                        medicalArtistsPictures = artData[i].pictures[0].large.url
+                        medicalArtistsTitle = artData[i].title.display
+                        medicalLat = artData[i].location.latitude
+                        medicalLong = artData[i].location.longitude
+                        medicalHood = artData[i].tours[0].name
+                     
+                        medicalArtArray.push(artData[i].exhibits[0].name + " " + artData[i].artists[0].name + " " + artData[i].pictures[0].large.url + " " + artData[i].title.display + " " + artData[i].location.latitude + " " + artData[i].location.longitude + " " + artData[i].tours[0].name + " " + artData[i].years[0].year);
+                     
+                    }
+                                  else if (artData[i].exhibits[0].name === "War"){
+                        console.log(artData[i].exhibits[0].name + " " + artData[i].artists[0].name + " " + artData[i].pictures[0].large.url + " " + artData[i].title.display + " " + artData[i].location.latitude + " " + artData[i].location.longitude + " " + artData[i].tours[0].name + " " + artData[i].years[0].year);
+                        warArtArray.push(artData[i].exhibits[0].name + " " + artData[i].artists[0].name + " " + artData[i].pictures[0].large.url + " " + artData[i].title.display + " " + artData[i].location.latitude + " " + artData[i].location.longitude + " " + artData[i].tours[0].name + " " + artData[i].years[0].year)
+                    }
+                  
+                 
+                
+              }
+        }
+            
+     })
+ }
+ getartData()
+
 //just testing to see how i can print items in the array seperately
 var fruits = ["Apple", "Banana","Pineapple"];
 fruits.forEach(function(fruit){ console.log(fruit) });
 
 
 
+
 //test
 
 
+
+function athleticfilter(afilter) {
+  return afilter === "Athletic";
+    
+}
+
 let submit = document.getElementById("submit")
 
+
 submit.addEventListener ("click", countries)
+
+
+console.log(exhibitArray)
+console.log(medicalArtArray)
+console.log(warArtArray)
+
+
+
+exhibitArray.forEach(function(element) {
+  console.log(element)
+})
+
+var athleticArts = exhibitArray.filter(name => name.includes('Athletic'))
+exhibitArray.forEach(element => console.log(element));
+
+
+function initMap() {
+    //map options
+    let options = {
+        zoom: 18,
+        center: {lat:39.9526, lng: -75.1652}, //map is centered around PHL
+    }
+    
+    // new map
+    let map = new google.maps.Map(document.getElementById('map'),options);
+     
+    
+    function addMarker (artArray) {
+     //   console.log(artArray)
+        let a = parseFloat(artArray.location.latitude);
+        let b = parseFloat(artArray.location.longitude);
+    
+        let marker = new google.maps.Marker({
+            position: {lat:a, lng:b},
+            map: map//what map do we want to add it to?
+            //icon: 'url for marker image we want to use'
+        });
+    }
+    
+        function addAthleticMarker (athleticArtArray) {
+        console.log(athleticArtArray)
+            
+          if (athleticArtArray.name === "Athletic") {
+        console.log("Button Works");
+        } else {
+        console.log("Not Athletic Category");
+        };
+    
+//        let a = parseFloat(artArray.location.latitude);
+//        let b = parseFloat(artArray.location.longitude);
+    
+//        let marker = new google.maps.Marker({
+//            position: {lat:a, lng:b},
+//            map: map//what map do we want to add it to?
+//            //icon: 'url for marker image we want to use'
+//        });
+    }
+    
+
+    //artArray()
+    
+    //adds markers for everything in array
+    for (let i=0;i<artArray.length; i++) {
+//        console.log(artArray[i])
+        addMarker(artArray[i])
+    }
+    
+    }
+
+
+
 
 function countries (){
     $.ajax({
